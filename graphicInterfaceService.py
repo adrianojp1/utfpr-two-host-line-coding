0fefe3a58e1790ab7f6b5110f5d3d257abc35b10
Matheus Diniz <67589319+matheusdfs@users.noreply.github.com>
2021-11-21 09:04:28 -0300

    Create graphicInterfaceService.py

---
+++ graphicInterfaceService.py
@@ -0,0 +1,83 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class graphicInterfaceService(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.tabelaValores = QtWidgets.QTableWidget(self.centralwidget)
+        self.tabelaValores.setGeometry(QtCore.QRect(40, 90, 711, 111))
+        self.tabelaValores.setMaximumSize(QtCore.QSize(711, 16777215))
+        self.tabelaValores.setObjectName("tabelaValores")
+        self.tabelaValores.setColumnCount(1)
+        self.tabelaValores.setRowCount(3)
+        item = QtWidgets.QTableWidgetItem()
+        self.tabelaValores.setVerticalHeaderItem(0, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tabelaValores.setVerticalHeaderItem(1, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tabelaValores.setVerticalHeaderItem(2, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tabelaValores.setHorizontalHeaderItem(0, item)
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(360, 10, 81, 31))
+        self.label.setTextFormat(QtCore.Qt.MarkdownText)
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(360, 50, 91, 31))
+        self.label_2.setTextFormat(QtCore.Qt.MarkdownText)
+        self.label_2.setObjectName("label_2")
+        self.grafico = QtWidgets.QGraphicsView(self.centralwidget)
+        self.grafico.setGeometry(QtCore.QRect(40, 270, 711, 241))
+        self.grafico.setObjectName("grafico")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(370, 230, 41, 31))
+        self.label_3.setTextFormat(QtCore.Qt.MarkdownText)
+        self.label_3.setObjectName("label_3")
+        self.botaoEnvio = QtWidgets.QPushButton(self.centralwidget)
+        self.botaoEnvio.setGeometry(QtCore.QRect(670, 210, 80, 21))
+        self.botaoEnvio.setObjectName("botaoEnvio")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 20))
+        self.menubar.setObjectName("menubar")
+        self.menuFile = QtWidgets.QMenu(self.menubar)
+        self.menuFile.setObjectName("menuFile")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.menubar.addAction(self.menuFile.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("Trabalho de comunicacao de dados - Equipe Minecraft", "Trabalho de comunicacao de dados - Equipe Minecraft"))
+        item = self.tabelaValores.verticalHeaderItem(0)
+        item.setText(_translate("MainWindow", "Mensagem escrita"))
+        item = self.tabelaValores.verticalHeaderItem(1)
+        item.setText(_translate("MainWindow", "Mensagem binário"))
+        item = self.tabelaValores.verticalHeaderItem(2)
+        item.setText(_translate("MainWindow", "Mensagem algoritmo"))
+        item = self.tabelaValores.horizontalHeaderItem(0)
+        item.setText(_translate("MainWindow", "Valores"))
+        self.label.setText(_translate("MainWindow", "Equipe Minecraft"))
+        self.label_2.setText(_translate("MainWindow", "Tabela de valores"))
+        self.label_3.setText(_translate("MainWindow", "Gráfico"))
+        self.botaoEnvio.setText(_translate("MainWindow", "Enviar"))
+        self.menuFile.setTitle(_translate("MainWindow", "File"))
+        self.botaoEnvio.clicked.connect(self.button_clicked)
+
+    def button_clicked(self):
+        print(self.tabelaValores.item(0, 0).text())
+
+    def set_binary_msg(self, value):
+        self.tabelaValores.setItem(1, 0, QtWidgets.QTableWidgetItem(value))
+
+    def set_algorithm_msg(self, value):
+        self.tabelaValores.setItem(2, 0, QtWidgets.QTableWidgetItem(value))
+
