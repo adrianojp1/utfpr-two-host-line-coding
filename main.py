4f59e3e173bdf986b8992f4588ab5a443f36f43f
Matheus Diniz <67589319+matheusdfs@users.noreply.github.com>
2021-11-21 09:07:36 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+﻿# This Python file uses the following encoding: utf-8
+import sys
+from PyQt5 import QtWidgets
+from PySide2.QtWidgets import QApplication
+from graphicInterfaceService import graphicInterfaceService
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = graphicInterfaceService()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
